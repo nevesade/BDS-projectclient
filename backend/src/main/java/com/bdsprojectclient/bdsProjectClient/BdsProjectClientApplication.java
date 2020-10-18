d977ccde6f5db6e10c2132b7a9318ba25b90937f
nevesade <adenasiocarvalho19@gmail.com>
2020-10-18 18:39:23 +0100

    Project created

---
+++ backend/src/main/java/com/bdsprojectclient/bdsProjectClient/BdsProjectClientApplication.java
@@ -0,0 +1,13 @@
+package com.bdsprojectclient.bdsProjectClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsProjectClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdsProjectClientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bdsprojectclient/bdsProjectClient/BdsProjectClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdsprojectclient.bdsProjectClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdsProjectClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
