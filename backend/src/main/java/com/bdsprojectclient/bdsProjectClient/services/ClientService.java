61e879b37e174eea8487004b3bd20f182f406864
nevesade <adenasiocarvalho19@gmail.com>
2020-10-19 22:41:26 +0100

    Delete Client

--- backend/src/main/java/com/bdsprojectclient/bdsProjectClient/services/ClientService.java
+++ backend/src/main/java/com/bdsprojectclient/bdsProjectClient/services/ClientService.java
@@ -8,12 +8,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bdsprojectclient.bdsProjectClient.dto.ClientDTO;
 import com.bdsprojectclient.bdsProjectClient.entities.Client;
 import com.bdsprojectclient.bdsProjectClient.repositories.ClientRepository;
+import com.bdsprojectclient.bdsProjectClient.services.serices.exceptions.DatabaseException;
 import com.bdsprojectclient.bdsProjectClient.services.serices.exceptions.ResourceNotFoundException;
 
 @Service
@@ -68,6 +71,19 @@ public class ClientService {
 			throw new ResourceNotFoundException("ID not found"  + id);
 		}
 		
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+			
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 }
